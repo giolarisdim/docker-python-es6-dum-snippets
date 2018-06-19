3fc07f3794e69bb997c90455201fd061d4e6ffc5
dimitris giolaris <>
2018-06-19 08:46:44 +0300

    turning messages from native to english

--- projectclass.py
+++ projectclass.py
@@ -8,7 +8,7 @@ import operator
 def readdetails():
     # { }
     print()
-    print ("give tha number of students mofo")
+    print ("please enter the number of students")
     numberofstudents = int(input())
     studentdetails = {}
     for students in range(0,numberofstudents):
@@ -27,22 +27,22 @@ def ranksort(studentdetails):
         print()
         sorted_studentdetails = sorted(studentdetails.items(), key=operator.itemgetter(1),reverse = True)
         print(sorted_studentdetails)
-        print ("o {} erxete protos me aponomi 500 eurw kai {} marks".format(sorted_studentdetails[0][0], sorted_studentdetails[0][1]))
-        print ("o {} erxete deuteros me aponomi 300 eurw kai {} marks".format(sorted_studentdetails[1][0], sorted_studentdetails[1][1]))
-        print ("o {} erxete tritos me aponomi 100 eurw kai {} marks".format(sorted_studentdetails[2][0], sorted_studentdetails[2][1]))
+        print (" {} is hitting the first place with a reward of 500 eurw and {} marks".format(sorted_studentdetails[0][0], sorted_studentdetails[0][1]))
+        print (" {} is hitting the second place with a reward of 300 eurw and {} marks".format(sorted_studentdetails[1][0], sorted_studentdetails[1][1]))
+        print (" {} is hitting the third place with a reward of 100 eurw and {} marks".format(sorted_studentdetails[2][0], sorted_studentdetails[2][1]))
         print()
         return sorted_studentdetails
     except IndexError:
-        print("ase tis maimoudies ponire")
+        print("aint no such thing as giving wrong numbers, enter the right one please")
         quit()
 
 
 def rewardstudent(sorted_studentdetails,reward):
     print()
 
-    print ("{} protos me {}".format(sorted_studentdetails[0][0],reward[0]))
-    print ("{} deuteros me {}".format(sorted_studentdetails[1][0],reward[1]))
-    print ("{} tritos me {}".format(sorted_studentdetails[2][0],reward[2]))
+    print ("{} first with {}".format(sorted_studentdetails[0][0],reward[0]))
+    print ("{} second with {}".format(sorted_studentdetails[1][0],reward[1]))
+    print ("{} third with {}".format(sorted_studentdetails[2][0],reward[2]))
     print()
 
 
@@ -51,7 +51,7 @@ def statement(sorted_studentdetails):
     print()
     for record in sorted_studentdetails:
         if record[1] >= 950:
-            print ("{} gg pires {} pontous".format(record[0], record[1]))
+            print ("{} congratulations! You allready have gather {} points".format(record[0], record[1]))
         else:
             break
     print()
